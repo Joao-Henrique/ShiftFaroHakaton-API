227688c3a8f7676196f80df653da2ce1bd0a2b9d
João Henrique <jppulicidade@gmail.com>
2018-03-22 13:07:00 +0000

    basic configuration for start testing with postman

--- Routes/api.js
+++ Routes/api.js
@@ -1,42 +1,46 @@
 const express = require('express');
 const router = express.Router();
-const ImageCardModel = require('../imageCardModel');
+const DataModel = require('../thisWillBeReplacedByTheDataModel'); // Replace this with the dataModel name
 
-// get a list of stored images from the db
-router.get('/storedImages', function (req, res, next) {
+// get a list of stored Data from the db
+router.get('/databaseFiles', function (req, res, next) {
   ImageCardModel.find({}).then(function (imageCard) {
     res.send(imageCard);
   })
 });
 
-// get a list of specific user stored images from the db
-router.get('/storedImages/:user', function (req, res, next) {
-  ImageCardModel.find({ user: req.params.user }).then(function (imageCard) {
+// get a list of specific user Data  from the db
+router.get('/databaseFiles/:dataType', function (req, res, next) {
+  ImageCardModel.find({ dataType: req.params.user }).then(function (imageCard) {
     res.send(imageCard);
   })
 });
 
 // add a new image to the db
-router.post('/storedImages', function (req, res, next) {
+router.post('/databaseFiles', function (req, res, next) {
   ImageCardModel.create(req.body).then(function (imageCard) {
     res.send(imageCard);
   }).catch(next);
 });
 
+
+
+/* UNCOMMENT IF YOU NEED OTHER METHODS */
+
 // update a image to the db
-router.put('/storedImages/:id', function (req, res, next) {
+/* router.put('/databaseFiles/:id', function (req, res, next) {
   ImageCardModel.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
     ImageCardModel.findOne({ _id: req.params.id }).then(function (imageCard) {
       res.send(imageCard);
     });
   });
-});
+}); */
 
 // delete a image from the db
-router.delete('/storedImages/:id', function (req, res, next) {
+/* router.delete('/databaseFiles/:id', function (req, res, next) {
   ImageCardModel.findByIdAndRemove({ _id: req.params.id }).then(function (imageCard) {
     res.send(imageCard);
   });
-});
+}); */
 
 module.exports = router;
